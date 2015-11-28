feb07748e0f0a174b4a9ff87ad24841e262ff0b8
Tas-sos <tasosnumberten@yahoo.gr>
2015-11-28 13:29:02 +0200

    Αρχείο εγκατάστασης

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@
 sudo mkdir /opt/ceidArtasIndicator
 sudo mv src images unistall.sh /opt/ceidArtasIndicator/
 
+sudo chmod o+x /opt/ceidArtasIndicator/unistall.sh
+
 # Για να προστεθεί το πρόγραμμα στα προγράμματα εκκίνησης, ούτως ώστε να ξεκινάει αυτόματα : 
 
 mv ciedArtas.desktop ~/.config/autostart
@@ -21,11 +23,9 @@ python3 /opt/ceidArtasIndicator/src/AnnouncementsForCEID-Artas.py &
 notify-send 'Announcements For CEID Artas' 'Η εγκατάσταση ολοκληρώθηκε επιτυχώς. Για ότι χρειαστείτε ήμαστε δίπλα σας.\nEυχαριστούμε πολύ. :)' -i /usr/share/icons/gnome/48x48/emblems/emblem-default.png
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 
-
-#rm README.md .gitignore
-# Για να διαγραφούν όλα τα αρχεία που κατέβασε ο χρήστης και είναι για την εγκατάταση :
 cd ..
 rm -R CEID-Artas_Not-Up-master
+rm CEID-Artas_Not-Up-master.zip
 
 
 
